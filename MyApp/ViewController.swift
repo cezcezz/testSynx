7b07d23199cff59c4bab000f0cb3628352378386
Cezar_ <vlsddm1@gmail.com>
2023-04-17 13:38:35 +0300

    initial commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Cezar_ on 17.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
